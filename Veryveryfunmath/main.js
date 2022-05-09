52ed1e0c5167b04a93a3f9e51ba718636fdaf5bf
alex9123 <101290286+alex9123@users.noreply.github.com>
2022-05-09 13:16:55 -0600

    Add files via upload

--- Veryveryfunmath/main.js
+++ Veryveryfunmath/main.js
@@ -23,79 +23,38 @@ checkButton.addEventListener("click", function() {
 })
 
 
-// Divide string into numbers and operator
-function separateNumbers(string) {
-    let equation = []
-    console.log(string.lastIndexOf(string.match(/\(([^)]+)\)/g)))
-    console.log(string.match(/\(([^)]+)\)/g))
+
+// calculate
+function calc(string) {
+    string = string.replaceAll("×", "*")
+    string = string.replaceAll("÷", "/")
+    string = string.replaceAll("sin", "Math.sin")
+    string = string.replaceAll("cos", "Math.cos")
+    string = string.replaceAll("tan", "Math.tan")
+
+    string = string.replaceAll("sin", "Math.sin")
     
-    equation = string.match(/[a-zA-Z]+|[()]+|[.0-9]+|[+-×÷]/g)
-   
-    console.log(equation)
-    return equation
+    console.log(string)
+
+    return Function('return ' + string)()
 }
 
 
-let superscript = false
-
 document.addEventListener("click", function(event) {
-    
     if (event.target.classList.contains("calc-button")) {
         if (event.target.innerHTML === "ac") {
             userCalcInput.innerHTML = ""
-            superscript = false
-        } else if(event.target.innerHTML === "=") {
-            
-            let equation = separateNumbers(userCalcInput.innerHTML)
-
-            // calculate */
-            for (i=0; i <= equation.length-1; i ++) {
-
-                if (equation[i] === "×") {
-                    newNum = +equation[i-1] * +equation[i+1]
-                    equation.splice(i-1, 3, newNum)
-                    i -= 2
-
-                } else if (equation[i] ==="÷") {
-                    newNum = +equation[i-1] / +equation[i+1]
-                    equation.splice(i-1, 3, newNum)
-                    i -= 2
-                }
-            }
-            // calculate +-
-            for (i=0; i <= equation.length-1; i ++) {
-
-                if (equation[i] === "+") {
-                    newNum = +equation[i-1] + +equation[i+1]
-                    equation.splice(i-1, 3, newNum)
-                    i -= 2
-
-                } else if (equation[i] ==="-") {
-                    newNum = +equation[i-1] - +equation[i+1]
-                    equation.splice(i-1, 3, newNum)
-                    i -= 2
-                }
-            }
-            userCalcInput.innerHTML = equation
-            superscript = false
+        } else if(event.target.innerHTML === "=") { 
+            userCalcInput.innerHTML = calc(userCalcInput.innerHTML)
         } else if(event.target.innerHTML.includes("x")) { 
-            if (event.target.innerHTML !== "" && !superscript) {
-                superscript = true
-            }
+            
         } else if(event.target.innerHTML === "del") {
-            let input = userCalcInput.innerHTML.split("")
-            console.log(input)
-            // userCalcInput.innerHTML = userCalcInput.innerHTML.slice(0, -1)
+            userCalcInput.innerHTML = userCalcInput.innerHTML.slice(0, -1)
             
         } else {
-            if (superscript && checkForNumbers(event.target.innerHTML)) {          
-                userCalcInput.innerHTML = userCalcInput.innerHTML + "<sup>" + event.target.innerHTML
-            } else {
-                superscript = false
-                userCalcInput.innerHTML = userCalcInput.innerHTML + event.target.innerHTML
-                if(event.target.classList[1] == "brackets") {
-                    userCalcInput.innerHTML = userCalcInput.innerHTML + "("
-                }
+            userCalcInput.innerHTML = userCalcInput.innerHTML + event.target.innerHTML
+            if(event.target.classList[1] == "brackets") {
+                userCalcInput.innerHTML = userCalcInput.innerHTML + "("
             }
         }
     }
